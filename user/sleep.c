8dc8e0bdbc2a5bedf42a1e6a5a13336ce8da57dd
linsole <linshunlong.o@outlook.com>
2021-07-11 17:14:50 +0800

    finish "sleep"

---
+++ user/sleep.c
@@ -0,0 +1,15 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+    if (argc <= 1) {
+        fprintf(2, "Failed to pass an argument!\n");
+        exit(1);
+    }
+    int n = atoi(argv[1]);
+    sleep(n);
+    exit(0);
+}
